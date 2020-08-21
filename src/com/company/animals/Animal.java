5f96458ae753eec89002892f2787d067dfd806fc
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-08-21 06:05:08 +0400

    L016 - Getters & Setters

--- src/com/company/animals/Animal.java
+++ src/com/company/animals/Animal.java
@@ -24,5 +24,35 @@ class Animal {
         this.currentPosition += length;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Integer currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    public Boolean getCanFly() {
+        return canFly;
+    }
+
+    public void setCanFly(Boolean canFly) {
+        this.canFly = canFly;
+    }
 }
