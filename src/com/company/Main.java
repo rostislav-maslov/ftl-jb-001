f497d5a678399001a457d47a3bb94a14183ce0ae
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-08-22 18:11:43 +0400

    no message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
 
 
-import com.company.L013.SwitchExample;
 import com.company.L020.TypeConversion;
 import com.company.L019.StringExample;
-import com.company.L026.FileExample;
 import com.company.animals.*;
 
 import java.io.IOException;
@@ -104,6 +102,5 @@ public class Main {
         } catch (Animal.WeightException ignore) {
         }
 
-        FileExample.run();
     }
 }
